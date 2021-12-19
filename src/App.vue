72d0f6a45ef3a7aae8eb83872e4d0a0022e4c0db
Martijn Dorsman <martijn.dorsman@gmail.com>
2021-12-19 13:00:37 +0100

    Init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+  <router-view></router-view>
+</div>
+</template>
+
+<style>
+
+</style>
